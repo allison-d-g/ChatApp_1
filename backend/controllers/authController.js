379addcb99c14d106ea0a6d06188583797f0f91d
allison-d-g <61395131+allison-d-g@users.noreply.github.com>
2024-04-17 20:20:32 -0700

    login, logout, signup working

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -9,18 +9,20 @@ export const signup = async (req,res) =>{
         if(password !== confirmPassword){
             return res.status(400).json({error:"Passwords do not match."});
         }
+        //check username
         const user = await User.findOne({username}); //check is username exist
         if(user){
-            return res.status(400).json({error:"username is already existed."});
+            return res.status(400).json({error:"username exists."});
         }
+
         //HASH Password
         const salt = await bcrypt.genSalt(10); // the higher the safe but slower
         //for security reason, the password will be encode in database
         const hashedPassword = await bcrypt.hash(password, salt); 
         
-        // http://avatar-placeholder.iran.liara.run/ //this is the web with random pic
-        const boyProfilePic = 'http://avatar.iran.liara.run/public/boy?username=${username}';
-        const girlProfilePic = 'http://avatar.iran.liara.run/public/girl?username=${username}';
+        // https://avatar-placeholder.iran.liara.run/ //this is the web with random pic
+        const boyProfilePic = 'https://avatar.iran.liara.run/public/boy?username=${username}';
+        const girlProfilePic = 'https://avatar.iran.liara.run/public/girl?username=${username}';
         
         const newUser = new User({
             fullName,
@@ -53,10 +55,39 @@ export const signup = async (req,res) =>{
     // console.log("Signup");
 };
 
-export const logout = (req,res) =>{
-    console.log("Logout");
+export const logout =  (req,res) =>{
+    try {
+        res.cookie("jwt", "", {maxAge: 0});
+        res.status(200).json({message:"Logged out successfully."});
+    } catch (error) {
+        console.log("Error in logout", error.message);
+        res.status(500).json({error:"Internal Server Error"});
+    }
 };
 
 export const login = async(req,res) =>{
-    console.log("login");
+   try {
+    const{username, password} =req.body; //input 
+    const user = await User.findOne({username}); //check username
+    //check if password correct
+    const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
+
+    if(!user || !isPasswordCorrect){
+        return res.status(400).json({error: "Invalid username or password."});
+    }
+
+    //log user in
+    generateTokenAndSetCookie(user._id, res);
+
+    res.status(200).json({
+        _id: user._id,
+        fullName: user.fullName,
+        username: user.username,
+        profilePic: user.profilePic,
+    }); //response
+
+   } catch (error) {
+        console.log("Error in login", error.message);
+        res.status(500).json({error:"Internal Server Error"});
+   }
 };
