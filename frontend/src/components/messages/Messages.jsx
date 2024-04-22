687bd8f595a34d23a8ce37cca2508174677c9683
allisondn <cadong@seas.upenn.edu>
2024-04-21 21:27:48 -0700

    all functions besides system greating work!

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import Message from './Message'
+import useGetMessages from '../../hooks/useGetMessages'
+import MessageSkeleton from "../skeletons/MessageSkeleton";
 
 const Messages = () => {
+  const {messages, loading} = useGetMessages();
+
+  //the scroll bar auto roll to the last message position
+  const lastMessage = useRef()
+
+  useEffect(() => {
+    setTimeout(() => {
+      lastMessage.current?.scrollIntoView({ behavior: "smooth" });
+    }, 50)
+  }, [messages])
+
+  console.log("messages: ", messages);
   return (
     <div className='px-4 flex-1 overflow-auto'>
+      {!loading &&
+				messages.length > 0 &&
+				messages.map((message) => (
+					<div key={message._id} ref={lastMessage}>
+						<Message message={message} />
+					</div>
+				))}
+
+      {loading && [...Array(0)].map((_, idx) => <MessageSkeleton key={idx} />)}
       {/* <h1>Message</h1> */}
-      <Message />
+      {/* <Message /> */}
     </div>
   )
 }
--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { TbMessageCircleSearch } from "react-icons/tb";
+import useConversation from '../../zustand/useConversation';
+import useGetConversations from "../../hooks/useGetConversations";
+// import setSelectedConversation from "../../zustand/useConversation"
+import toast from 'react-hot-toast';
 
+//this implements the search function, user can search name in the search bar
 const SearchInput = () => {
+  const [search, setSearch] = useState("");
+  const{ setSelectedConversation } = useConversation();
+  const{ conversations} = useGetConversations();
+
+  const handleSubmit =(e) => {
+    e.preventDefault()
+    if (!search) return;
+		if (search.length < 3) {
+			return toast.error("Search term must be at least 3 characters long");
+		}
+
+		const conversation = conversations.find((c) => c.fullName.toLowerCase().includes(search.toLowerCase()));
+
+		if (conversation) {
+			setSelectedConversation(conversation);
+			setSearch("");
+		} else toast.error("No such user!");
+  }
+
   return (
     // <div><h1>Hello</h1></div>
-    <form className='flex items-center gap-2'>
-        <input type='text' placeholder='Search…' className='input input-bordered rounded-full' />
+    <form onSubmit = {handleSubmit} className='flex items-center gap-2'>
+        <input type='text' placeholder='Search…' className='input input-bordered rounded-full'
+          value={search}
+          onChange={(e) => setSearch(e.target.value)} />
         <button type='submit' className='btn btn-circle bg-sky-500 text-white'>
             <TbMessageCircleSearch />
         </button>  
